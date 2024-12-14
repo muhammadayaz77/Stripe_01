9ae9117f1c2d36cf95f6295bf4dafc87d042a276
Muhammad Ayaz <muhammadayaz22757@gmail.com>
2024-12-14 21:29:56 +0500

    add for multiple data

--- controllers/paymentController.mjs
+++ controllers/paymentController.mjs
@@ -3,33 +3,64 @@ let stripe = Stripe('sk_test_51QVmyJGO9zJL4clfK58gcpU9wmCgbtWI2Po8Z0rYLP8N2Cu8l3
 
 export let paymenCheckout = async (req,res)=>{
   try {
-    // Using directly provided data from req.body
     const session = await stripe.checkout.sessions.create({
-      line_items: [
-        {
+      line_items: req.body.products.map((product) => {
+        return {
           price_data: {
             currency: "USD",
             product_data: {
-              name: req.body.name,
+              name: product.name,
+              //   description: product.description,
             },
-            unit_amount: req.body.unit_amount * 100, // Stripe requires the price in cents
+            unit_amount: product.unit_amount * 100,
           },
-          quantity: req.body.quantity,
-        },
-      ],
+          quantity: product.quantity,
+        };
+      }),
       mode: "payment",
       success_url: "http://localhost:5173/success",
       cancel_url: "http://localhost:5173/cancel",
     });
-
     res.json({
       success: true,
       session,
     });
   } catch (error) {
-    res.status(500).json({
+    return res.json({
       success: false,
       message: error.message,
     });
   }
 }
+
+
+// try {
+    // Using directly provided data from req.body
+  //   const session = await stripe.checkout.sessions.create({
+  //     line_items: [
+  //       {
+  //         price_data: {
+  //           currency: "USD",
+  //           product_data: {
+  //             name: req.body.name,
+  //           },
+  //           unit_amount: req.body.unit_amount * 100, // Stripe requires the price in cents
+  //         },
+  //         quantity: req.body.quantity,
+  //       },
+  //     ],
+  //     mode: "payment",
+  //     success_url: "http://localhost:5173/success",
+  //     cancel_url: "http://localhost:5173/cancel",
+  //   });
+
+  //   res.json({
+  //     success: true,
+  //     session,
+  //   });
+  // } catch (error) {
+  //   res.status(500).json({
+  //     success: false,
+  //     message: error.message,
+  //   });
+  // }
